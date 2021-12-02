ae273d9ccf9c6d62b72ca425efd09cac2285f49f
Agata Szulc <agataszulc97@gmail.com>
2021-12-02 15:40:14 +0100

    first scrollanim works - yoyo

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { interval, Subscription } from 'rxjs';
+import { gsap, TimelineMax } from 'gsap';
+import ScrollMagic from 'ScrollMagic';
 
 @Component({
   selector: 'app-main-page',
@@ -19,9 +21,36 @@ export class MainPageComponent implements OnInit {
 
     this.imageIndex = 0;
 
-    this.imagesCycleSubscription = interval(5000).subscribe((val) => {
-      this.cycleImage();
-    });
+    // var menuAnim = gsap.from('img', {
+    //   // scrollTrigger: {
+    //   //   trigger: '#img',
+    //   //   // start: 'top center',
+    //   //   // markers: true,
+    //   //   toggleActions: 'play pause reverse reset',
+    //   // },
+    //   x: 1300,
+    //   duration: 5,
+    //   stagger: 0.3,
+    // });
+    // menuAnim.pause();
+
+    const tween = gsap.timeline({ paused: true, repeat: -1, yoyo: true });
+    var controller = new ScrollMagic.Controller({});
+    tween.to('.main', 1, { left: '10%', xPercent: -10 });
+    new ScrollMagic.Scene({
+      triggerElement: '#img',
+      duration: '100%',
+    })
+      .on('enter', function () {
+        tween.play();
+      })
+      .on('leave', function () {
+        tween.pause();
+      })
+      .addTo(controller);
+    // this.imagesCycleSubscription = interval(5000).subscribe((val) => {
+    //   this.cycleImage();
+    // });
   }
 
   fetchListOfImages(): void {
