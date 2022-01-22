9d4aacf23c4f5f159b286b4e7eee5795372d6190
adelajaworowska <adela.jaworowska@gmail.com>
2022-01-22 20:58:34 +0100

    Grid is not working because of test css class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { LeftSideComponent } from './left-side/left-side.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    LeftSideComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
